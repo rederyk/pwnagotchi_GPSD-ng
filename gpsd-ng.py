646b51d4393ee4eac7b69c1594b86117fa99c32d
frédéric <frederic.matray@me.com>
2025-03-07 18:47:23 +0100

    Version update

--- gpsd-ng.py
+++ gpsd-ng.py
@@ -581,7 +581,7 @@ class GPSD_ng(plugins.Plugin):
     __name__: str = "GPSD-ng"
     __GitHub__: str = "https://github.com/fmatray/pwnagotchi_GPSD-ng"
     __author__: str = "@fmatray"
-    __version__: str = "1.7.2"
+    __version__: str = "1.7.3"
     __license__: str = "GPL3"
     __description__: str = (
         "Use GPSD server to save position on handshake. Can use mutiple gps device (serial, USB dongle, phone, etc.)"
